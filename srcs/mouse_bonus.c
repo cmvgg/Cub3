8af122510ee5d399825d7cb6ef965f1726baace9
ivromero <ivromero42@icloud.com>
2025-01-24 18:05:38 +0100

    limpieza y arreglar mouse

--- srcs/mouse_bonus.c
+++ srcs/mouse_bonus.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   mouse_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivromero <ivromero@student.42urduliz.c>    +#+  +:+       +#+        */
+/*   By: ivromero <ivromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 23:40:34 by ivromero          #+#    #+#             */
-/*   Updated: 2025/01/12 19:19:17 by ivromero         ###   ########.fr       */
+/*   Updated: 2025/01/24 18:01:16 by ivromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+
+#ifdef BONUS
 
 static int	bonus_mouse_move(int x, int y, t_data *dsp_dt)
 {
@@ -22,18 +24,16 @@ static int	bonus_mouse_move(int x, int y, t_data *dsp_dt)
 	if (x != center_x)
 	{
 		if (x > center_x)
-			rot_right(rot_speed, dsp_dt);
+			rot_left(rot_speed, dsp_dt);
 		else if (x < center_x)
-			rot_left(-rot_speed, dsp_dt);
+			rot_right(-rot_speed, dsp_dt);
 		mlx_mouse_move(dsp_dt->mlx.mlx, dsp_dt->mlx.win, center_x,
 			dsp_dt->screen_h / 2);
 	}
 	(void)y;
-	printf("x: %d, y: %d\n", x, y);
 	return (0);
 }
 
-#ifdef BONUS
 
 void	bonus_init_mouse(t_data *dsp_dt)
 {
@@ -48,6 +48,5 @@ void	bonus_init_mouse(t_data *dsp_dt)
 void	bonus_init_mouse(t_data *dsp_dt)
 {
 	(void)dsp_dt;
-	(void)bonus_mouse_move;
 }
 #endif
