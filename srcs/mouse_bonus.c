fb9edd6740a6752fff7293d7f06e13d4028982eb
ivromero <ivromero42@icloud.com>
2025-01-24 23:02:34 +0100

    mouse fix

--- srcs/mouse_bonus.c
+++ srcs/mouse_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouse_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivromero <ivromero@student.42urduliz.c>    +#+  +:+       +#+        */
+/*   By: ivromero <ivromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 23:40:34 by ivromero          #+#    #+#             */
-/*   Updated: 2025/01/12 19:19:17 by ivromero         ###   ########.fr       */
+/*   Updated: 2025/01/24 23:01:03 by ivromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ static int	bonus_mouse_move(int x, int y, t_data *dsp_dt)
 	double	rot_speed;
 
 	center_x = dsp_dt->screen_w / 2;
-	rot_speed = 0.003 * (x - center_x);
+	rot_speed = 0.001 * (x - center_x);
 	if (x != center_x)
 	{
 		if (x > center_x)
-			rot_right(rot_speed, dsp_dt);
+			rot_left(rot_speed, dsp_dt);
 		else if (x < center_x)
-			rot_left(-rot_speed, dsp_dt);
+			rot_right(-rot_speed, dsp_dt);
 		mlx_mouse_move(dsp_dt->mlx.mlx, dsp_dt->mlx.win, center_x,
 			dsp_dt->screen_h / 2);
 	}
